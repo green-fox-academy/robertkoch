7e329c55542da56c3f10a5ad25e399c9c870eb97
robert.koch <robert.koch.hu1@gmail.com>
2018-08-08 13:31:00 +0200

    horizontal finished

--- robertkochhu/week02/day3/Horizontal/src/Drawing.java
+++ robertkochhu/week02/day3/Horizontal/src/Drawing.java
@@ -4,14 +4,24 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Drawing {
+public class Drawing{
     public static void mainDraw(Graphics graphics) {
         // Create a line drawing function that takes 2 parameters:
         // The x and y coordinates of the line's starting point
         // and draws a 50 long horizontal line from that point.
         // Draw 3 lines with that function. Use loop for that.
+        int c =80;
+        int d=60;
+
+        for(int i =0;i<3;i++){
+        lineFinisher(c,d,graphics);
+        d+=20;c+=10;}}
 
 
+
+
+    public static void lineFinisher(int a,int b,Graphics graphics){
+        graphics.drawLine(a,b,a+50,b);
 
     }
 
