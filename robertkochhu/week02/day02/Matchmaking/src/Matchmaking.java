b22ecd843e897f7250f6bb13827ab5fbdacb4434
robert.koch <robert.koch.hu1@gmail.com>
2018-08-08 09:01:45 +0200

    tasks finished

--- robertkochhu/week02/day02/Matchmaking/src/Matchmaking.java
+++ robertkochhu/week02/day02/Matchmaking/src/Matchmaking.java
@@ -4,17 +4,19 @@ public class Matchmaking {
     public static void main(String[] args) {
         ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Claire", "Kat", "Jane"));
         ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Tom", "Todd", "Neef", "Jeff"));
-        ArrayList<String> lista1 = new ArrayList<String>,
+        System.out.println(matchMakingSystem(girls,boys));
     }
 
     public static ArrayList<String> matchMakingSystem(ArrayList<String> lista1, ArrayList<String> lista2) {
-        ArrayList<String> commonList = new ArrayList<String>;
-        for (int i = 0; i <= 5; i++) {
-            for (int j =0;j<<=5;j++)
+        ArrayList<String> commonList = new ArrayList<String>();
+        for (int i = 0; i < 5; i++) {
+            commonList.add(lista1.get(i));
+            commonList.add(lista2.get(i));
         }
+        return commonList;
 
     }
 
 
 }
-}
+
---
+++ robertkochhu/week02/day02/isinlist/src/isinlist.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class isinlist{
+    public static void main(String... args){
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+        checkNums(list);
+        // Check if list contains all of the following elements: 4,8,12,16
+        // Create a method that accepts list as an input
+        // it should return "true" if it contains all, otherwise "false"
+        System.out.println(checkNums(list));
+
+    }
+    private static boolean checkNums(ArrayList<Integer> list) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(4, 8, 12, 16));
+        if (list.containsAll(numbers)) return true;
+        else return false;
+}}
