9dbf4b1e68f42e8037327e477cc0f699b5e4fbb1
robert.koch <robert.koch.hu1@gmail.com>
2018-08-11 23:31:09 +0200

    DiagonalMatrix

---
+++ robertkochhu/week02/day1/Matrix/src/Matrix.java
@@ -0,0 +1,12 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+
+            System.out.println();
+        }
+    }
+}
