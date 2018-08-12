e25710ad3cb855164705f178108f9981bf26bfa5
robert.koch <robert.koch.hu1@gmail.com>
2018-08-12 13:12:23 +0200

    unique

--- robertkochhu/week02/day1/Unique/src/Unique.java
+++ robertkochhu/week02/day1/Unique/src/Unique.java
@@ -1,10 +1,25 @@
 import java.util.*;
 
 public class Unique {
-    public static void main(String[] args){
-    int[] numbers={1,1,5,8,5,7,7,9,45,9};
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 5, 8, 5, 7, 7, 9, 45, 9};
+        unique1(numbers);
     }
-    public static void unique1(int[] a){
+
+    public static void unique1(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            boolean isUnique = false;
+            for (int j = 0; j < i; j++) {
+                if (a[i] == a[j]) {
+                    isUnique = true;
+                    break;
+                }
+
+            }
+            if (isUnique == false) {
+                System.out.print(a[i] + ",");
+            }
+        }
 
     }
 }
