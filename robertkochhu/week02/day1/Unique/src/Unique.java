ec022c2e48ebcddbcc5bc05b3f8e0614a2c3ca32
robert.koch <robert.koch.hu1@gmail.com>
2018-08-12 10:54:09 +0200

    push

---
+++ robertkochhu/week02/day1/Unique/src/Unique.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Unique {
+    public static void main(String[] args){
+    int[] numbers={1,1,5,8,5,7,7,9,45,9};
+    }
+    public static void unique1(int[] a){
+
+    }
+}
