90473b37fc74b3d6f1f50d1564d293bca4b48c95
robert.koch <robert.koch.hu1@gmail.com>
2018-08-14 14:57:43 +0200

    countlines

--- robertkochhu/week03/day1/CountLines/src/CountLines.java
+++ robertkochhu/week03/day1/CountLines/src/CountLines.java
@@ -1,3 +1,27 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class CountLines {
-    public static
+    public static void main(String[] args) {
+        String filename = "asd.txt";
+
+        System.out.println(countLines(filename));
+    }
+
+    public static int countLines(String name) {
+        int lineAmount = 0;
+        try {
+            Path filePath = Paths.get(name);
+            List<String> lines = Files.readAllLines(filePath);
+            lineAmount = lines.size();
+            return lineAmount;
+        } catch (Exception e) {
+
+            return 0;
+        }
+
+
+    }
 }
