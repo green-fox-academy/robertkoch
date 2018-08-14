b7ccf699db00e21e5fb2f2c21d096152b1e54a09
robert.koch <robert.koch.hu1@gmail.com>
2018-08-14 17:30:26 +0200

    w3d2finished

---
+++ robertkochhu/week03/day1/Log/src/Log.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Log {
+    public static void main(String[] args) {
+        List<String> content = new ArrayList();
+        try {
+            Path logPlace = Paths.get("https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/file-manipulations/logs/log.txt");
+            List<String> lines = Files.readAllLines(logPlace);
+
+            for(int i =0;logPlace.)
+
+
+        }catch(IOException e){
+
+        }
+    }
+}
