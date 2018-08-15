b56a597e60a69ec99df3ad53f88516093356c82a
robert.koch <robert.koch.hu1@gmail.com>
2018-08-15 19:48:42 +0200

    sharpie finished

---
+++ robertkochhu/week03/day3/Sharpie/src/Sharpie.java
@@ -0,0 +1,13 @@
+public class Sharpie {
+    String color;
+    float width;
+    float inkAmount;
+    Sharpie(String color, float width){
+        this.color=color;
+        this.width=width;
+        this.inkAmount=100;
+    }
+    public void use(){
+        this.inkAmount --;
+    }
+}
