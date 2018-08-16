8140d486cf62ac9aba3e0c130947ef18695866ac
robert.koch <robert.koch.hu1@gmail.com>
2018-08-16 14:57:37 +0200

    bunnyline

---
+++ robertkochhu/week03/day4/BunnyLine/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+        int bunnies = 100;
+        System.out.println(earCounter(bunnies));
+    }
+    public static int earCounter(int bunnies){
+
+        if (bunnies ==1){
+            return 2;
+        }
+        else if(bunnies % 2 ==0){
+            int earAmount =earCounter(bunnies-1)+2;
+            return  earAmount;
+        }
+        else {
+            int earAmount =earCounter(bunnies-1)+3;
+            return  earAmount;
+
+        }
+    }
+}
