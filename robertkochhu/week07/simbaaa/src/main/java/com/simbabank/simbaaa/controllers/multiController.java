34a54a5eb798b25aa5cc1f4f53948fa18dc3d4bd
robert.koch <robert.koch.hu1@gmail.com>
2018-09-18 17:33:33 +0200

    prettify finished

---
+++ robertkochhu/week07/simbaaa/src/main/java/com/simbabank/simbaaa/controllers/multiController.java
@@ -0,0 +1,23 @@
+package com.simbabank.simbaaa.controllers;
+
+import com.simbabank.simbaaa.models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class multiController {
+    List<BankAccount> list = new ArrayList<>();
+    @RequestMapping("/multiAccount")
+    public String multiAccountShow(Model model) {
+        list.add(new BankAccount("Vuk",100,"fox",false,true));
+        list.add(new BankAccount("Frakk",1100,"dog",false,false));
+        list.add(new BankAccount("MekkElek",0,"goat",false,true));
+        list.add(new BankAccount("Pumba",999,"Phacochoerus aethiopicus",true,true));
+        model.addAttribute("list",list);
+        return "multiAccount";
+    }
+}
--- robertkochhu/week07/simbaaa/src/main/java/com/simbabank/simbaaa/models/BankAccount.java
+++ robertkochhu/week07/simbaaa/src/main/java/com/simbabank/simbaaa/models/BankAccount.java
@@ -6,11 +6,29 @@ public class BankAccount {
     private String name;
     private int balance;
     private String animalType;
+    private boolean isKing;
+    private boolean isGood;
 
     public BankAccount(String name, int balance, String animalType) {
         this.name = name;
         this.balance = balance;
         this.animalType = animalType;
+        this.isKing=false;
+    }
+
+    public BankAccount(String name, int balance, String animalType, boolean isKing) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+        this.isKing = isKing;
+    }
+
+    public BankAccount(String name, int balance, String animalType, boolean isKing, boolean isGood) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+        this.isKing = isKing;
+        this.isGood = isGood;
     }
 
     public String getName() {
@@ -35,5 +53,21 @@ public class BankAccount {
 
     public void setAnimalType(String animalType) {
         this.animalType = animalType;
+    }
+
+    public boolean isKing() {
+        return isKing;
+    }
+
+    public void setKing(boolean king) {
+        isKing = king;
+    }
+
+    public boolean isGood() {
+        return isGood;
+    }
+
+    public void setGood(boolean good) {
+        isGood = good;
     }
 }
