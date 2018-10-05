6a2d46ef78e35c9f2c11061706a4d285acaf1008
robert.koch <robert.koch.hu1@gmail.com>
2018-10-05 09:15:23 +0200

    sql database created

---
+++ robertkochhu/orient-exam/src/main/java/greenfox/tourist/TouristApplication.java
@@ -0,0 +1,12 @@
+package greenfox.tourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristApplication.class, args);
+    }
+}
