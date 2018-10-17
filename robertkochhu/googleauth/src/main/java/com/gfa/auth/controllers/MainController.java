5f296428f39307a0c40095148de099cb4ca9ee40
robert.koch <robert.koch.hu1@gmail.com>
2018-10-17 13:58:48 +0200

    authentication

---
+++ robertkochhu/googleauth/src/main/java/com/gfa/auth/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.gfa.auth.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return ("index");
+    }
+    @GetMapping("/stuff")
+    public String stuffFunction() {
+        return ("stuff");
+    }
+}
